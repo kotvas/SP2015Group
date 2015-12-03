b2fe420a74035050878a247b74cc58a88eb14a4f
kotv17@gmail.com <kotv17@gmail.com>
2015-12-03 20:29:40 +0200

    MyFirstGITSolution

---
+++ MyFirstGITSolution/MyFirstGITSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstGITSolution.Startup))]
+namespace MyFirstGITSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
