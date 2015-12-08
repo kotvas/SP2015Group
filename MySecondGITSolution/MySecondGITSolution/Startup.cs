fd1d685f0b696daac7b5ab56126fffcdc3a25d0e
kotv17@gmail.com <kotv17@gmail.com>
2015-12-08 21:50:55 +0200

    second git solution

---
+++ MySecondGITSolution/MySecondGITSolution/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySecondGITSolution.Startup))]
+namespace MySecondGITSolution
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
